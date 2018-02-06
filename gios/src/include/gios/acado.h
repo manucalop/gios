b2ac09a1185a7b40dd8477dd289ce34220099f1d
Manuel Castillo <manuelcloop@tutamail.com>
2018-02-06 15:05:18 +0100

    Fixing acado solver bugs

--- gios/src/include/gios/acado.h
+++ gios/src/include/gios/acado.h
@@ -17,17 +17,17 @@ namespace gios{
       AcadoSolver();
       ~AcadoSolver();
     public:
-      void linkState(         gios::VariablePtr &var, const unsigned &step, const unsigned &pos) override;
-      void linkControl(       gios::VariablePtr &var, const unsigned &step, const unsigned &pos) override;
-      void linkReference(     gios::VariablePtr &var, const unsigned &step, const unsigned &pos) override;
-      void linkWeight(        gios::VariablePtr &var, const unsigned &step, const unsigned &pos) override;
-      void linkParameter(     gios::VariablePtr &var, const unsigned &step, const unsigned &pos) override;
+      void linkState(         VariablePtr &var, const unsigned &step, const unsigned &pos) override;
+      void linkControl(       VariablePtr &var, const unsigned &step, const unsigned &pos) override;
+      void linkReference(     VariablePtr &var, const unsigned &step, const unsigned &pos) override;
+      void linkWeight(        VariablePtr &var, const unsigned &step, const unsigned &pos) override;
+      void linkParameter(     VariablePtr &var, const unsigned &step, const unsigned &pos) override;
 
-      void linkFeedbackState( gios::VariablePtr &var, const unsigned &pos) override;
-      void linkEndReference(  gios::VariablePtr &var, const unsigned &pos) override;
-      void linkEndWeight(     gios::VariablePtr &var, const unsigned &pos) override;
+      void linkFeedbackState( VariablePtr &var, const unsigned &pos) override;
+      void linkEndReference(  VariablePtr &var, const unsigned &pos) override;
+      void linkEndWeight(     VariablePtr &var, const unsigned &pos) override;
 
-      void getParameters(gios::Parameters &p) override;
+      void getParameters(Parameters &p) override;
       unsigned getN() override;
 
       void solve() override;
@@ -36,6 +36,24 @@ namespace gios{
 
 AcadoSolver::AcadoSolver(){//{{{
   acado_initializeSolver();
+  // Initialize States
+  for (unsigned i = 0; i < ACADO_NX * (ACADO_N + 1); ++i)  acadoVariables.x[ i ] = 0.0;
+  // Initialize Controls
+  for (unsigned i = 0; i < ACADO_NU * ACADO_N; ++i)  acadoVariables.u[ i ] = 0.0;
+  // Initialize online data
+  for (unsigned i = 0; i < ACADO_NOD * (ACADO_N + 1); ++i)  acadoVariables.od[ i ] = 0.0;
+  // Initialize weights (all weights have to be at least 1!!!
+  for (unsigned i = 0; i < ACADO_NY*ACADO_NY; ++i)  acadoVariables.W[ i ] = 0.0;
+  //for( unsigned i=0; i<ACADO_NY; i++){
+	// 	acadoVariables.W[ ( i )*(ACADO_NY+1) ] = 1.0;
+  //}
+  //for (unsigned i = 0; i < ny*ny*n; ++i)  acadoVariables.W[ i ] = 0.0;
+  for (unsigned i = 0; i < ACADO_NYN*ACADO_NYN; ++i)  acadoVariables.WN[ i ] = 0.0;
+  // Initialize the reference. 
+  for (unsigned i = 0; i < ACADO_NY * ACADO_N; ++i)  acadoVariables.y[ i ] = 0.0;
+  for (unsigned i = 0; i < ACADO_NYN; ++i)  acadoVariables.yN[ i ] = 0.0;
+  //Initialize the current state feedback. 
+  for (unsigned i = 0; i < ACADO_NX; ++i) acadoVariables.x0[ i ] = 0.0;
   std::cout<<"Acado Solver Created"<<std::endl;
 }//}}}
 
@@ -43,39 +61,39 @@ AcadoSolver::~AcadoSolver(){//{{{
   std::cout<<"Acado Solver Deleted"<<std::endl;
 };//}}}
 
-void AcadoSolver::linkState(gios::VariablePtr &var, const unsigned &step, const unsigned &pos){//{{{
+void AcadoSolver::linkState(VariablePtr &var, const unsigned &step, const unsigned &pos){//{{{
     var = &acadoVariables.x[step*ACADO_NX + pos];
 }//}}}
 
-void AcadoSolver::linkFeedbackState(gios::VariablePtr &var, const unsigned &pos){//{{{
+void AcadoSolver::linkFeedbackState(VariablePtr &var, const unsigned &pos){//{{{
     var = &acadoVariables.x0[pos];
 }//}}}
 
-void AcadoSolver::linkControl(gios::VariablePtr &var, const unsigned &pos, const unsigned &step){//{{{
+void AcadoSolver::linkControl(VariablePtr &var, const unsigned &step, const unsigned &pos){//{{{
     var = &acadoVariables.u[step*ACADO_NU + pos];
 }//}}}
 
-void AcadoSolver::linkReference(gios::VariablePtr &var, const unsigned &pos, const unsigned &step){//{{{
+void AcadoSolver::linkReference(VariablePtr &var, const unsigned &step, const unsigned &pos){//{{{
     var = &acadoVariables.y[step*ACADO_NY + pos];
 }//}}}
 
-void AcadoSolver::linkWeight(gios::VariablePtr &var, const unsigned &step, const unsigned &pos){//{{{
+void AcadoSolver::linkWeight(VariablePtr &var, const unsigned &step, const unsigned &pos){//{{{
   var = &acadoVariables.W[step*ACADO_NY*ACADO_NY + pos*(ACADO_NY +1)];
 }//}}}
 
-void AcadoSolver::linkParameter(gios::VariablePtr &var,const unsigned &step, const unsigned &pos){//{{{
+void AcadoSolver::linkParameter(VariablePtr &var, const unsigned &step, const unsigned &pos){//{{{
     var = &acadoVariables.od[step*ACADO_NOD + pos];
 }//}}}
 
-void AcadoSolver::linkEndReference(gios::VariablePtr &var, const unsigned &pos){//{{{
+void AcadoSolver::linkEndReference(VariablePtr &var, const unsigned &pos){//{{{
     var = &acadoVariables.yN[pos];
 }//}}}
 
-void AcadoSolver::linkEndWeight(gios::VariablePtr &var, const unsigned &pos){//{{{
+void AcadoSolver::linkEndWeight(VariablePtr &var, const unsigned &pos){//{{{
     var = &acadoVariables.WN[pos*(ACADO_NYN + 1)];
 }//}}}
 
-void AcadoSolver::getParameters(gios::Parameters &p){//{{{
+void AcadoSolver::getParameters(Parameters &p){//{{{
   p.n   = ACADO_N;
   p.nx  = ACADO_NX;  //Number of differential states 
   p.nu  = ACADO_NU;  //Number of controls
